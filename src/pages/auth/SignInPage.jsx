63a187353bbf3976bdc000033cd24b3ceefcb1a0
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-10-11 18:35:19 -0700

    project init

---
+++ src/pages/auth/SignInPage.jsx
@@ -0,0 +1,10 @@
+const SignInPage = () => {
+  return (
+    <>
+      <div className="font-playfair text-3xl">sign In!</div>
+      <i className="fa-solid fa-star p-4 text-2xl animate-spin" />
+    </>
+  )
+}
+
+export default SignInPage
---
+++ src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <div className="font-playfair text-3xl">sign up!</div>
+}
+
+export default SignUpPage
