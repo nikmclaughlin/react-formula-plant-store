e330e7c733674aeb362806c85f9200958254202e
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-10-17 22:44:52 -0700

    fetch plant data

--- src/pages/auth/PlantListPage/index.jsx
+++ src/pages/auth/PlantListPage/index.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from 'react'
+import * as plantService from 'services/plant'
 import NavBar from 'sharedComponents/NavBar'
 import RedirectToSignInIfSignedOut from 'sharedComponents/RedirectToSignInIfSignedOut'
 
 const PlantListPage = () => {
+  useEffect(() => {
+    ;(async () => {
+      const response = await plantService.getPlants()
+      const data = await response.json()
+      console.log(data)
+    })()
+  }, [])
+
   return (
     <RedirectToSignInIfSignedOut>
       <NavBar />
