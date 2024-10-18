07d45158e2aee817809126f677fee01b104fdc31
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-10-17 22:44:45 -0700

    include session auth headers

--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,4 +1,5 @@
 const { VITE_API_BASE_URL, VITE_API_KEY } = import.meta.env
+import * as userService from 'services/user'
 
 const apiFetch = (method, path, body = null) => {
   const options = {
@@ -8,6 +9,11 @@ const apiFetch = (method, path, body = null) => {
       Authorization: 'Bearer ' + VITE_API_KEY,
       'Content-Type': 'application/json',
     },
+  }
+
+  const sessionToken = userService.getSessionTokenStorage()
+  if (sessionToken) {
+    options.headers['Capstone-Session'] = sessionToken
   }
 
   if (body) {
